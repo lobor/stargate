31a503992676196654868de6772731a46993e29f
Lionel Bertrand <lionel.bertrand.prestataire@sfr.com>
2016-08-31 16:13:44 +0200

    delete file

--- web/src/utils/Api.js
+++ web/src/utils/Api.js
@@ -1,13 +1,5 @@
-// var g = window.io.connect();
-// console.log(socket);
-// console.log(window.io);
 var io = window.io.connect();
-io.on('login', function(...args){
-  console.log(this);
-  console.log(...args);
-});
-// socket.emit('login');
-// socket.emit('my other event', {});
+
 module.exports = {
   run: function(name, data, cb){
     io.emit(name, data, cb);
