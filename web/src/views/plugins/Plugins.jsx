1e9469e7875ac3a64720f777aede479c0f42983c
Lionel Bertrand <lionel.bertrand.prestataire@sfr.com>
2016-10-07 14:46:10 +0200

    t

--- web/src/views/plugins/Plugins.jsx
+++ web/src/views/plugins/Plugins.jsx
@@ -2,6 +2,9 @@
 import { CardStyle, CardTitleStyle } from './style';
 
 class Plugins extends React.Component {
+  componentWillMount(){
+    let request = new XMLHttpRequest();
+  }
   render() {
     return (
 			<div>Plugins</div>
